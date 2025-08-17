f1db5645b006cd39ceef52f4f7539c9838d62275
fuyb <656452024@qq.com>
2025-08-17 20:54:38 +0800

    update

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
-  description = "Dev shell flake for fcitx5-android prebuilder";
+  description = "Linux build environment for fcitx5";
 
-  inputs.fcitx5-android.url = "github:fcitx5-android/fcitx5-android";
+  inputs.fcitx5-android.url = "file:///prebuilder/fcitx5-android";
   inputs.flake-compat = {
     url = "github:edolstra/flake-compat";
     flake = false;
@@ -12,13 +12,10 @@
       nixpkgs = fcitx5-android.inputs.nixpkgs;
       pkgs = import nixpkgs {
         system = "x86_64-linux";
-        config.android_sdk.accept_license = true;
-        config.allowUnfree = true;
         overlays = [ fcitx5-android.overlays.default ];
       };
     in with pkgs;
     let
-      sdk = pkgs.fcitx5-android.sdk;
       prebuilder = haskellPackages.callPackage ./nix { };
       prebuilderShell = (haskell.lib.addBuildTools prebuilder [
         haskell-language-server
@@ -26,35 +23,37 @@
         cabal-install
       ]).env;
     in {
-      devShells.x86_64-linux.default = (sdk.shell.override {
-        androidStudio = null;
-        generateLocalProperties = false;
-      }).overrideAttrs (old: {
-        ABI = "armeabi-v7a,arm64-v8a,x86,x86_64";
-        ANDROID_PLATFORM = sdk.platformVersion;
-        CMAKE_VERSION = sdk.cmakeVersion;
-        shellHook = ''
-          ${old.shellHook}
-          export ANDROID_NDK_ROOT="$ANDROID_SDK_ROOT/ndk/${sdk.ndkVersion}"
-          export COMP_SPELL_DICT="${fcitx5}/lib/fcitx5/libexec/comp-spell-dict"
-        '';
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        name = "fcitx5-linux-dev";
 
-        buildInputs = old.buildInputs ++ [
-          fcitx5
-          opencc
+        # 标准 Linux 构建依赖
+        nativeBuildInputs = [
+          cmake
+          pkg-config
           autoconf
           automake
-          pkg-config
           libtool
+        ];
+
+        # 运行时依赖
+        buildInputs = [
+          fcitx5
+          opencc
           check
           zstd
           boost
         ] ++ prebuilderShell.buildInputs;
 
-        nativeBuildInputs = old.nativeBuildInputs
-          ++ prebuilderShell.nativeBuildInputs;
+        # 添加 Haskell 工具链
+        inputsFrom = [ prebuilderShell ];
 
-      });
+        # 设置纯 Linux 环境变量
+        shellHook = ''
+          export COMP_SPELL_DICT="${fcitx5}/lib/fcitx5/libexec/comp-spell-dict"
+          echo "Linux build environment for fcitx5 ready"
+        '';
+      };
+
       packages.x86_64-linux.default =
         haskell.lib.justStaticExecutables prebuilder;
     };
